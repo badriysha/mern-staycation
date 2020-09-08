4e35c7f51980c6839ddfcb75a2f9f549ca0f7f7b
badriysha <badriysha@gmail.com>
2020-09-08 12:41:06 +0700

    FInished creating Footer component + add react-reveal fade effect

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 
 import ImageHero from "assets/images/img-hero.jpg";
 import Frame from "assets/images/img-hero-frame.jpg";
@@ -18,91 +19,95 @@ export default function Hero(props) {
 
   return (
     <section className="container pt-4">
-      <div className="row align-items-center">
-        <div className="col-auto pr-5" style={{ width: 500 }}>
-          <h1 className="line-height-1 mb-4 font-weight-bold">
-            Forget Busy Work,
-            <br />
-            Start Next Vacation
-          </h1>
-          <p
-            className="font-weight-light text-gray-500 mb-4 w-75"
-            style={{ lineHeight: "170%" }}
-          >
-            We provide what you need to enjoy your holiday with family. Time to
-            make another memorable moments.
-          </p>
-          <Button
-            className="btn px-5 mt-2"
-            style={{ marginBottom: 80 }}
-            isPrimary
-            hasShadow
-            onClick={showMostPicked}
-          >
-            Show Me Now
-          </Button>
+      <Fade bottom>
+        <div className="row align-items-center">
+          <div className="col-auto pr-5" style={{ width: 500 }}>
+            <h1 className="line-height-1 mb-4 font-weight-bold">
+              Forget Busy Work,
+              <br />
+              Start Next Vacation
+            </h1>
+            <p
+              className="font-weight-light text-gray-500 mb-4 w-75"
+              style={{ lineHeight: "170%" }}
+            >
+              We provide what you need to enjoy your holiday with family. Time
+              to make another memorable moments.
+            </p>
+            <Button
+              className="btn px-5 mt-2"
+              style={{ marginBottom: 80 }}
+              isPrimary
+              hasShadow
+              onClick={showMostPicked}
+            >
+              Show Me Now
+            </Button>
 
-          <div className="row mt-5">
-            <div className="col-auto" style={{ marginRight: 35 }}>
-              <img
-                width="32"
-                height="32"
-                src={IconTravel}
-                alt={`${props.data.travelers} travelers`}
-              />
-              <h6 className="mt-3">
-                {formatNumber(props.data.travelers)}{" "}
-                <span className="text-gray-500 font-weight-light">
-                  travelers
-                </span>
-              </h6>
+            <div className="row mt-5">
+              <div className="col-auto" style={{ marginRight: 35 }}>
+                <img
+                  width="32"
+                  height="32"
+                  src={IconTravel}
+                  alt={`${props.data.travelers} travelers`}
+                />
+                <h6 className="mt-3">
+                  {formatNumber(props.data.travelers)}{" "}
+                  <span className="text-gray-500 font-weight-light">
+                    travelers
+                  </span>
+                </h6>
+              </div>
+              <div className="col-auto" style={{ marginRight: 35 }}>
+                <img
+                  width="32"
+                  height="32"
+                  src={IconTreasure}
+                  alt={`${props.data.treasures} treasures`}
+                />
+                <h6 className="mt-3">
+                  {formatNumber(props.data.treasures)}{" "}
+                  <span className="text-gray-500 font-weight-light">
+                    treasures
+                  </span>
+                </h6>
+              </div>
+              <div className="col-auto">
+                <img
+                  width="32"
+                  height="32"
+                  src={IconCities}
+                  alt={`${props.data.cities} cities`}
+                />
+                <h6 className="mt-3">
+                  {formatNumber(props.data.cities)}{" "}
+                  <span className="text-gray-500 font-weight-light">
+                    cities
+                  </span>
+                </h6>
+              </div>
             </div>
-            <div className="col-auto" style={{ marginRight: 35 }}>
+          </div>
+
+          <div className="col-6 pl-5">
+            <div style={{ width: 520, height: 410 }}>
               <img
-                width="32"
-                height="32"
-                src={IconTreasure}
-                alt={`${props.data.treasures} treasures`}
+                src={ImageHero}
+                alt="Hero"
+                className="img-fluid position-absolute"
+                style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
               />
-              <h6 className="mt-3">
-                {formatNumber(props.data.treasures)}{" "}
-                <span className="text-gray-500 font-weight-light">
-                  treasures
-                </span>
-              </h6>
-            </div>
-            <div className="col-auto">
               <img
-                width="32"
-                height="32"
-                src={IconCities}
-                alt={`${props.data.cities} cities`}
+                src={Frame}
+                alt="Hero Frame"
+                className="img-fluid position-absolute"
+                style={{ margin: "0 -15px -15px 0" }}
               />
-              <h6 className="mt-3">
-                {formatNumber(props.data.cities)}{" "}
-                <span className="text-gray-500 font-weight-light">cities</span>
-              </h6>
             </div>
           </div>
         </div>
-
-        <div className="col-6 pl-5">
-          <div style={{ width: 520, height: 410 }}>
-            <img
-              src={ImageHero}
-              alt="Hero"
-              className="img-fluid position-absolute"
-              style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
-            />
-            <img
-              src={Frame}
-              alt="Hero Frame"
-              className="img-fluid position-absolute"
-              style={{ margin: "0 -15px -15px 0" }}
-            />
-          </div>
-        </div>
-      </div>
+      </Fade>
     </section>
   );
 }
--- src/parts/Testimonial.js
+++ src/parts/Testimonial.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 
 import Star from "elements/Star";
 import Button from "elements/Button";
@@ -26,26 +27,28 @@ export default function Testimonial({ data }) {
           </div>
         </div>
         <div className="col">
-          <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
-          <Star value={data.rate} width={35} height={35} spacing={4}></Star>
-          <h5 className="h2 font-weight-light line-height-2 my-3">
-            {data.content}
-          </h5>
-          <span className="text-gray-500">
-            {data.familyName}, {data.familyOccupation}
-          </span>
-          <div>
-            <Button
-              className="btn px-5"
-              style={{ marginTop: 40 }}
-              isPrimary
-              hasShadow
-              type="link"
-              href={`/testimonial/${data._id}`}
-            >
-              Read Their Story
-            </Button>
-          </div>
+          <Fade right>
+            <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
+            <Star value={data.rate} width={35} height={35} spacing={4}></Star>
+            <h5 className="h2 font-weight-light line-height-2 my-3">
+              {data.content}
+            </h5>
+            <span className="text-gray-500">
+              {data.familyName}, {data.familyOccupation}
+            </span>
+            <div>
+              <Button
+                className="btn px-5"
+                style={{ marginTop: 40 }}
+                isPrimary
+                hasShadow
+                type="link"
+                href={`/testimonial/${data._id}`}
+              >
+                Read Their Story
+              </Button>
+            </div>
+          </Fade>
         </div>
       </div>
     </section>
